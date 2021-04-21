ecebda28271ebf58316a945354b0ef0e542ed7b6
trevista01 <trevista01@DESKTOP-I4IE7I4>
2021-04-21 20:03:31 +0530

    patient profile changes

--- src/app/patients/settings/settings.component.ts
+++ src/app/patients/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ToastrService } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder,
+    private auth: AuthenticationService, private toastr: ToastrService,
+    private userService: UserService, private date: DatePipe) { }
+  patientprofileForm: FormGroup;
+  files: File[] = [];
   ngOnInit(): void {
+    this.patientprofileForm = this.fb.group({
+      id: [0, Validators.required],     
+      email: [{ value: this.auth.userValue.email, disabled: true }, [Validators.required]],
+      firstName: [this.auth.userValue.firstName, [Validators.required]],
+      lastName: [this.auth.userValue.lastName, [Validators.required]],
+      phoneNumber: ['', [Validators.required]],     
+      dateOfBirth: ['', [Validators.required]],
+      bloodGroup: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      zipCode: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+     
+    });
   }
-
+  onSubmit() {
+    console.log(this.patientprofileForm);
+  }
 }
--- src/app/patients/settings/settings.module.ts
+++ src/app/patients/settings/settings.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
-  ]
+    SettingsRoutingModule, FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [DatePipe]
 })
 export class SettingsModule { }
